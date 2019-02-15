9fc83e543fb9335e1b8cb02c8f6a1d325d8f6154
Oliver Siegemund <osiegemund@rosen-group.com>
2019-02-15 21:48:40 +0100

    Demo App mit System.Task

---
+++ App1/App1/FirstViewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace App1
+{
+    public partial class FirstViewController : UIViewController
+    {
+        Task<double> randomRunner;
+        public FirstViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void Slider_OnSlidingEnd(UIKit.UISlider sender)
+        {
+            this.Number.Text = sender.Value.ToString();
+        }
+
+        public async void RunNumberUpdate()
+        {
+            var doubleValue = await randomRunner;
+            this.RandomNumber.Text = doubleValue.ToString();
+        } 
+
+        partial void On_DoStuffButtonPressed(UIKit.UIButton sender)
+        {
+            this.randomRunner = Task.Factory.StartNew<double>((controlerObject) => {
+                var controler = controlerObject as FirstViewController;
+                double num = 0.0;
+                if (controler != null)
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        num = new Random().NextDouble();
+                        Thread.Sleep(600);
+                    }
+                }
+                return num;
+            }, this, TaskCreationOptions.LongRunning);
+            this.RunNumberUpdate();
+        }
+
+
+    }
+}
---
+++ App1/App1/FirstViewController.designer.cs
@@ -0,0 +1,54 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace App1
+{
+    [Register ("FirstViewController")]
+    partial class FirstViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField Number { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField RandomNumber { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider Slider { get; set; }
+
+        [Action ("On_DoStuffButtonPressed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void On_DoStuffButtonPressed (UIKit.UIButton sender);
+
+        [Action ("Slider_OnSlidingEnd:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Slider_OnSlidingEnd (UIKit.UISlider sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Number != null) {
+                Number.Dispose ();
+                Number = null;
+            }
+
+            if (RandomNumber != null) {
+                RandomNumber.Dispose ();
+                RandomNumber = null;
+            }
+
+            if (Slider != null) {
+                Slider.Dispose ();
+                Slider = null;
+            }
+        }
+    }
+}
